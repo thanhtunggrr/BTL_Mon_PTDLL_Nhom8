43752f45d66521a0eff2601b0352f1cef929ab41
thanhtunggrr <150771980+thanhtunggrr@users.noreply.github.com>
2026-03-12 23:03:18 +0700

    Update and rename MoiTruongThucAn.py to ThucAn.py

--- ThucAn.py
+++ ThucAn.py
@@ -14,18 +14,13 @@ habitat_percent = [60, 25, 15]
 
 fig = plt.figure(figsize=(8,4), facecolor='white')
 
-# Biểu đồ 1: Thức ăn
+
 plt.subplot(1,2,1)
 colors_food = ["#6A5ACD", "#FF8C00", "#20B2AA"]  # màu lạ
-plt.pie(food_percent, labels=foods, autopct='%1.0f%%', colors=colors_food, startangle=90)
+plt.pie(food_percent, labels=foods, autopct='%1.0f%%', colors=colors_food, 
+        startangle=90)
 plt.title("Thức ăn của chim cánh cụt")
 
-# Biểu đồ 2: Môi trường sống
-plt.subplot(1,2,2)
-colors_habitat = ["#00CED1", "#9370DB", "#FF7F50"]
-plt.bar(habitats, habitat_percent, color=colors_habitat)
-plt.title("Môi trường sống của chim cánh cụt")
-plt.ylabel("Tỷ lệ (%)")
 
 plt.tight_layout()
 
