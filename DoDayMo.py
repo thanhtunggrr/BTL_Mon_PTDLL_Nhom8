b7f5a261c92da495e4950824e75d2a109782f62f
thanhtunggrr <150771980+thanhtunggrr@users.noreply.github.com>
2026-03-14 22:45:15 +0700

    Update DoDayMo.py

--- DoDayMo.py
+++ DoDayMo.py
@@ -1,6 +1,7 @@
 # 1. Import thư viện
 import seaborn as sns
 import matplotlib.pyplot as plt
+import networkx as nx
 
 # 2. Tải dữ liệu chim cánh cụt
 penguins = sns.load_dataset("penguins")
@@ -19,19 +20,37 @@ print("Lớn nhất:", bill_depth.max())
 
 # 6. Tính trung bình theo từng loài
 bill_depth_species = penguins.groupby("species")["bill_depth_mm"].mean()
+
 print("\nĐộ dày mỏ trung bình theo loài:")
 print(bill_depth_species)
 
-# 7. Vẽ biểu đồ
+# 7. Tạo graph hierarchy
+G = nx.DiGraph()
+
+# Nút gốc
+G.add_node("Penguins")
+
+# Thêm các loài làm nhánh
+for species, value in bill_depth_species.items():
+    label = f"{species}\n{round(value,2)} mm"
+    G.add_node(label)
+    G.add_edge("Penguins", label)
+
+# 8. Vẽ biểu đồ
 plt.figure(figsize=(6,4))
 
-plt.bar(bill_depth_species.index, bill_depth_species.values)
+pos = nx.spring_layout(G, seed=42)
 
-plt.title("Độ dày mỏ trung bình của các loài chim cánh cụt")
-plt.xlabel("Loài chim cánh cụt")
-plt.ylabel("Độ dày mỏ (mm)")
+nx.draw(
+    G,
+    pos,
+    with_labels=True,
+    node_color="lightblue",
+    node_size=2500,
+    font_size=9
+)
 
-plt.grid(axis="y", linestyle="--", alpha=0.5)
+plt.title(" độ dày mỏ trung bình của các loài chim cánh cụt")
 
 plt.tight_layout()
 plt.show()
